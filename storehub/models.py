c6a5b99157ef7cbd1c35be6cff1996c3bd26f7d6
hussamElmaghraby <hussamelmaghrabi@gmail.com>
2025-05-22 20:33:50 +0300

    order models.py

--- storehub/models.py
+++ storehub/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -18,3 +19,15 @@ class Product(models.Model):
     image  = models.ImageField(upload_to='products_images/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    # for lazy references 'Product'
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orders')
+    user = models.ForeignKey(User , on_delete=models.CASCADE, related_name='orders')
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username} for {self.product.name}"

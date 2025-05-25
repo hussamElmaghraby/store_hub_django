15d78a74e09fc5ac6485e13eacbb5a51b1fa237d
hussamElmaghraby <hussamelmaghrabi@gmail.com>
2025-05-25 18:50:38 +0300

    order item

--- storehub/models.py
+++ storehub/models.py
@@ -16,18 +16,29 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    image  = models.ImageField(upload_to='products_images/', blank=True)
+    image = models.ImageField(upload_to='products_images/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Order(models.Model):
     # for lazy references 'Product'
-    product =  models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orders')
-    user = models.ForeignKey(User , on_delete=models.CASCADE, related_name='orders')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orders')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     quantity = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     ordered_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username} for {self.product.name}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=0)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
--- storehub/serializers.py
+++ storehub/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Product, Category, Order
+from .models import Product, Category, Order, OrderItem
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -29,6 +29,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
         # execute save mehtod + hashing the password text
     def create(self, validated_data):
+        iems_data = validated_data.pop('items')
         return User.objects.create_user(**validated_data)
 
 
@@ -38,3 +39,14 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'product', 'user', 'quantity', 'total_price', 'ordered_at']
         read_only_fields = ['user' , 'total_price', 'ordered_at']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = [ 'quantity' , 'price', 'product']
+
+
+
+
+
